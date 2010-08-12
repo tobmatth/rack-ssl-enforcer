997d7d26dee8981e4c78271c596c1e235ea8283b
Rémy Coutable <rymai@rymai.com>
2010-08-12 10:06:51 +0200

    Optimizations. Don't set @options[:redirect_to] and @options[:message] when protocol is already https

--- lib/rack-ssl-enforcer.rb
+++ lib/rack-ssl-enforcer.rb
@@ -1,25 +1,28 @@
 module Rack
-
+  
   class SslEnforcer
-
+    
     def initialize(app, options = {})
-      @app = app
+      @app     = app
       @options = options
     end
- 
-    def call(env)
-      @options[:redirect_to] ||= Rack::Request.new(env).url
-      @options[:redirect_to].gsub!(/^http:/,'https:')
-      @options[:message] ||= "You are beeing redirected to #{@options[:redirect_to]}."
-      ssl_request?(env) ? @app.call(env) : [301, {'Location' => @options[:redirect_to]}, @options[:message]]
-    end
     
+    def call(env)
+      if ssl_request?(env)
+        @app.call(env)
+      else
+        @options[:redirect_to] ||= Rack::Request.new(env).url
+        @options[:redirect_to].gsub!(/^http:/, 'https:')
+        @options[:message] ||= "You are beeing redirected to #{@options[:redirect_to]}."
+        [301, { 'Location' => @options[:redirect_to] }, @options[:message]]
+      end
+    end
     
     private
       
       def ssl_request?(env)
         (env['HTTP_X_FORWARDED_PROTO'] || env['rack.url_scheme']) == 'https'
       end
- 
+      
   end
 end
