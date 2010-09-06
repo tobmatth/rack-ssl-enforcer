3f40e58911d8447c896a18d5746e50eb11f31f2c
Thibaud Guillaume-Gentil <thibaud@thibaud.me>
2010-09-06 10:10:56 +0200

    Avoid cache on Heroku

--- lib/rack/ssl-enforcer.rb
+++ lib/rack/ssl-enforcer.rb
@@ -16,7 +16,15 @@ module Rack
       if scheme
         @options[:redirect_to] ||= "#{scheme}://#{@req.host}#{@req.path}"
         @options[:message] ||= "#{Time.now} - #{env['PATH_INFO']} - #{@req.path} You are being redirected to #{@options[:redirect_to]}."
-        [307, { 'Content-Type' => 'text/plain', 'Location' => @options[:redirect_to] }, [@options[:message]].flatten]
+        [
+          307,
+          { 
+            'Content-Type'  => 'text/plain',
+            'Cache-Control' => 'no-cache',
+            'Location'      => @options[:redirect_to]
+          },
+          [@options[:message]].flatten
+        ]
       else
         @app.call(env)
       end
