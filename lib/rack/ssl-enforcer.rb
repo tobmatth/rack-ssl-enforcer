6773e2f6d9aa4735a23db3a68e6cde404ec33079
Paul Annesley <paul@annesley.cc>
2010-11-16 23:42:57 +1100

    Redirect URL always uses default port for scheme.
    Given that a hostname cannot run HTTP and HTTPS on a single port,
    the port of the original request should not be preserved in the
    redirect location URL.
    
    Previous behavior:
    http://example.org:81/ => https://example.org:81/
    https://example.org:4000/ => http://example.org:4000/
    
    Updated behavior:
    http://example.org:81/ => https://example.org/
    https://example.org:4000/ => http://example.org/

--- lib/rack/ssl-enforcer.rb
+++ lib/rack/ssl-enforcer.rb
@@ -14,7 +14,7 @@ module Rack
       end
       
       if scheme
-        location = @options[:redirect_to] || @req.url.gsub(/^https?/, scheme)
+        location = @options[:redirect_to] || replace_scheme(@req, scheme).url
         body     = "<html><body>You are being <a href=\"#{location}\">redirected</a>.</body></html>"
         [301, { 'Content-Type' => 'text/html', 'Location' => location }, [body]]
       else
@@ -43,5 +43,15 @@ module Rack
       end
     end
     
+    def replace_scheme(req, scheme)
+      Rack::Request.new(req.env.merge(
+        'rack.url_scheme' => scheme,
+        'SERVER_PORT' => port_for(scheme).to_s
+      ))
+    end
+    
+    def port_for(scheme)
+      scheme == 'https' ? 443 : 80
+    end
   end
 end
